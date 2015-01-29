91539c648c42c79fa583df5bc0310b50f309a573
jaedb <james@barnsley.nz>
2015-01-29 16:51:29 +1300

    Adding curl error catcher

--- authenticate.php
+++ authenticate.php
@@ -1,3 +1,31 @@
+<html>
+<head>
+	<title>Spotmop authentication with SpotifyAPI</title>
+	
+	<style>
+		
+		* {
+			border: 0;
+			padding: 0;
+			margin: 0;
+		}
+		
+		html, body {
+			background: #EEEEEE;
+			font-size: 16px;
+			font-family: Arial, Helvetica;
+			color: #555555;
+		}
+		
+		body {
+			padding: 20px;
+			text-align: center;
+		}
+		
+	</style>
+	
+</head>
+<body>
 <?php
 
 /*
@@ -9,6 +37,9 @@ function getToken($code){
 	
 	$ch = curl_init();
 
+	if (FALSE === $ch)
+		throw new Exception('Failed to initialize');
+
 	$post_data = array(
 			'client_id' => 'a87fb4dbed30475b8cec38523dff53e2',
 			'client_secret' => 'd7c89d0753ef4068bba1678c6cf26ed6',
@@ -16,14 +47,24 @@ function getToken($code){
 			'code' => $code,
 			'redirect_uri' => 'http://'.$_SERVER["SERVER_NAME"].substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/"))
 		);
-
+	
 	curl_setopt($ch, CURLOPT_URL,"https://accounts.spotify.com/api/token");
 	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
-
+	curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	
 	$response = curl_exec($ch);
-	curl_close ($ch);
+	
+	if(curl_errno($ch)){
+		echo 'CURL Rrror: '. curl_error($ch);
+	}
+	
+	curl_close($ch);
 	
 	return $response;
 }
@@ -64,6 +105,7 @@ if( isset($_GET['code']) ){
 	
 	// let's save our authorization code to local storage for future use
 	?>
+		<h3>Completing authorization ...</h3>
 		<script type="text/javascript">
 			localStorage.authorization_code = '<?php echo $_GET['code']; ?>';
 		</script>
@@ -75,7 +117,7 @@ if( isset($_GET['code']) ){
 	// now let's parse the access token back to the js application
 	?>
 		<script type="text/javascript">
-			var response = <?php echo $response ?>;
+			var response = <?php echo ( $response ) ? $response : '""' ?>;
 			
 			localStorage.refresh_token = response.refresh_token;
 			localStorage.access_token = response.access_token;
