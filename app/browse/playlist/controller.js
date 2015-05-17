d46a8f10ea57a01d4db5e224812f6e1dde724570
James Barnsley <james@barnsley.nz>
2015-05-17 20:48:42 +1200

    Templates

--- app/browse/playlist/controller.js
+++ app/browse/playlist/controller.js
@@ -15,11 +15,19 @@ angular.module('spotmop.browse.playlist', [
 		
 	$scope.playlist = {};
 	$scope.tracks = {};
+	$scope.totalTime = 0;
 	
 	SpotifyService.getPlaylist( $routeParams.uri )
 		.success(function( response ) {
 			$scope.playlist = response;
 			$scope.tracks = response.tracks;
+			
+			// figure out the total time for all tracks
+			var totalTime = 0;
+			$.each( $scope.tracks.items, function( key, track ){
+				totalTime += track.track.duration_ms;
+			});	
+			$scope.totalTime = Math.round(totalTime / 100000);
 		})
 		.error(function( error ){
 			$scope.status = 'Unable to load new releases';
