22dcae209f3bf739ecce0d9f5802faf5222ae466
James Barnsley <james@barnsley.nz>
2015-05-18 07:58:33 +1200

    Adding foggy

--- app/browse/playlist/controller.js
+++ app/browse/playlist/controller.js
@@ -17,6 +17,9 @@ angular.module('spotmop.browse.playlist', [
 	$scope.tracks = {};
 	$scope.totalTime = 0;
 	
+	// todo: blur out background images in banners. Make it cross-browser too.
+	console.log( $(document).find('.playlist-intro .image').foggy() );
+	
 	SpotifyService.getPlaylist( $routeParams.uri )
 		.success(function( response ) {
 			$scope.playlist = response;
