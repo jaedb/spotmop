72479516a4c1a6d37f1e61aeacafcda5e4765bbb
James Barnsley <james@barnsley.nz>
2015-07-31 09:10:43 +1200

    Experimenting with CSS parallax ... no luck yet

--- app/browse/artist/controller.js
+++ app/browse/artist/controller.js
@@ -96,27 +96,7 @@ angular.module('spotmop.browse.artist', [])
                                 });
 						});
 				});
-		});
-    
-    // on scroll, create some nice parallax for the banner image
-    $('#body').on('scroll', function(evt){
-        
-        // get our ducks in a row - these are all the numbers we need
-        var scrollPosition = $(this).scrollTop();
-		var banner = $(document).find('.artist-intro > .image-container');
-        var bannerHeight = banner.outerHeight();
-		
-		// if we have the banner in the viewport
-		if( scrollPosition <= bannerHeight ){
-			
-			// calculate how far down the banner we've scrolled
-			var percent = -( scrollPosition / bannerHeight * 15 );
-			
-			// and finally apply to the image
-			banner.find('.image').css({ top: -( percent * 10 ) });
-		}
-    });
-	
+		});	
 })
 
 /**
