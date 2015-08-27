08a2ec6059448da6c7b02382925634f4876ff10c
James Barnsley <james@barnsley.nz>
2015-08-28 10:08:14 +1200

    Moving search into own page; Need to handle when no  parsed (ie when nav clicked)

--- app/search/controller.js
+++ app/search/controller.js
@@ -6,7 +6,7 @@ angular.module('spotmop.search', [])
 .config(function($stateProvider) {
 	$stateProvider
 		.state('search', {
-			url: "/search/:query",
+			url: "/search/:type/:query",
 			templateUrl: "app/search/template.html",
 			controller: 'SearchController'
 		});
@@ -15,35 +15,115 @@ angular.module('spotmop.search', [])
 /**
  * Main controller
  **/
-.controller('SearchController', function SearchController( $scope, $rootScope, $stateParams, $timeout, SpotifyService ){
+.controller('SearchController', function SearchController( $scope, $rootScope, $state, $stateParams, $timeout, SpotifyService ){
 	
 	$scope.query = $stateParams.query;
 	$scope.tracklist = {tracks: []};
 	$scope.albums = [];
 	$scope.artists = [];
 	$scope.playlists = [];
+	$scope.type = $stateParams.type;	
+	$scope.loading = false;
 	
-	SpotifyService.getSearchResults( 'track', $stateParams.query, 20 )
-		.success( function(response){
+	$scope.dropdownActive = false;
+	$scope.toggleDropdown = function(){
+		if( $scope.dropdownActive )
+			$scope.dropdownActive = false;
+		else
+			$scope.dropdownActive = true;
+	}
+	
+	/**
+	 * Watch our query string for changes
+	 * When changed, clear all results, wait for 0.5 seconds for next key, then fire off the search
+	 **/
+	$scope.$watch('[query]', function (){
+		$scope.loading = true;
+		$scope.tracklist = {tracks: []};
+		$scope.albums = [];
+		$scope.artists = [];
+		$scope.playlists = [];
+		$timeout(
+			function(){
+				$state.go('search',{ type: $scope.type, query: $scope.query});			
+			}, 500
+		);
+	}, true);
+	
+	
+	/**
+	 * On init, figure out what type of results we want
+	 * This defines the type of search requests we'll perform, and thus the page layout
+	 **/
+	switch( $stateParams.type ){
 		
-			$scope.tracklist = response.tracks;
-			$scope.tracklist.tracks = response.tracks.items;
-			
-			SpotifyService.getSearchResults( 'album', $stateParams.query, 6 )
+		case 'track' :
+			$scope.loading = true;
+			SpotifyService.getSearchResults( 'track', $stateParams.query, 50 )
 				.success( function(response){
-				
-					$scope.albums = response.albums;
-					SpotifyService.getSearchResults( 'artist', $stateParams.query, 6 )
-						.success( function(response){
-						
-							$scope.artists = response.artists;
-								SpotifyService.getSearchResults( 'playlist', $stateParams.query, 6 )
-									.success( function(response){
-									
-										$scope.playlists = response.playlists;
-										$rootScope.$broadcast('spotmop:pageUpdated');
-									});
-						});
+					$scope.tracklist = response.tracks;
+					$scope.tracklist.tracks = response.tracks.items;
+					$rootScope.$broadcast('spotmop:pageUpdated');
+					$scope.loading = false;
 				});
-		});	
+			break;
+		
+		case 'album' :
+			$scope.loading = true;
+			SpotifyService.getSearchResults( 'album', $stateParams.query, 20 )
+				.success( function(response){		
+					$scope.albums = response.albums;
+					$rootScope.$broadcast('spotmop:pageUpdated');
+					$scope.loading = false;
+				});
+			break;
+				
+		case 'artist' :
+			$scope.loading = true;
+			SpotifyService.getSearchResults( 'artist', $stateParams.query, 20 )
+				.success( function(response){		
+					$scope.artists = response.artists;
+					$rootScope.$broadcast('spotmop:pageUpdated');
+					$scope.loading = false;
+				});
+			break;
+				
+		case 'playlist' :
+			$scope.loading = true;
+			SpotifyService.getSearchResults( 'playlist', $stateParams.query, 20 )
+				.success( function(response){		
+					$scope.playlists = response.playlists;
+					$rootScope.$broadcast('spotmop:pageUpdated');
+					$scope.loading = false;
+				});
+			break;
+		
+		default :
+			$scope.loading = 4;
+			SpotifyService.getSearchResults( 'track', $stateParams.query, 20 )
+				.success( function(response){
+					$scope.tracklist = response.tracks;
+					$scope.tracklist.tracks = response.tracks.items;
+					$scope.loading -= 1;
+				});	
+				
+			SpotifyService.getSearchResults( 'album', $stateParams.query, 6 )
+				.success( function(response){		
+					$scope.albums = response.albums;
+					$scope.loading -= 1;
+				});
+				
+			SpotifyService.getSearchResults( 'artist', $stateParams.query, 6 )
+				.success( function(response){		
+					$scope.artists = response.artists;
+					$scope.loading -= 1;
+				});
+				
+			SpotifyService.getSearchResults( 'playlist', $stateParams.query, 6 )
+				.success( function(response){		
+					$scope.playlists = response.playlists;
+					$scope.loading -= 1;
+				});
+			break;
+	}
 });
