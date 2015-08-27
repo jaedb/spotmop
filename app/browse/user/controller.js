5873a4c8f845138a27bd3f2d26e7c62139143a02
Mopidy Server <james@barnsley.nz>
2015-08-27 06:36:09 +0000

    Adding load on demand for users page

--- app/browse/user/controller.js
+++ app/browse/user/controller.js
@@ -32,7 +32,9 @@ angular.module('spotmop.browse.user', [])
             // get their playlists
             SpotifyService.getPlaylists( response.id )
                 .success(function( response ) {
-                    $scope.playlists = response;
+                    $scope.playlists = response.items;
+                    $scope.next = response.next;
+                    $scope.totalPlaylists = response.total;
 					$rootScope.$broadcast('spotmop:pageUpdated');
                     $rootScope.$broadcast('spotmop:notifyUserRemoval', {id: 'loading-user'});
                 })
@@ -45,4 +47,50 @@ angular.module('spotmop.browse.user', [])
             $rootScope.$broadcast('spotmop:notifyUserRemoval', {id: 'loading-user'});
             $rootScope.$broadcast('spotmop:notifyUser', {type: 'bad', id: 'loading-user', message: error.error.message});
         });
+    
+    /**
+     * Load more of the user's playlists
+     * Triggered by scrolling to the bottom
+     **/
+    
+    var loadingMorePlaylists = false;
+    
+    // go off and get more of this playlist's tracks
+    function loadMorePlaylists( $nextUrl ){
+        
+        if( typeof( $nextUrl ) === 'undefined' )
+            return false;
+        
+        // update our switch to prevent spamming for every scroll event
+        loadingMorePlaylists = true;   
+        
+        $rootScope.$broadcast('spotmop:notifyUser', {type: 'loading', id: 'loading-more-playlists', message: 'Loading more'});
+
+        // go get our 'next' URL
+        SpotifyService.getUrl( $nextUrl )
+            .success(function( response ){
+            
+                // append these new playlists to our existing array
+                $scope.playlists = $scope.playlists.concat( response.items );
+                
+                // save the next set's url (if it exists)
+                $scope.next = response.next;
+                
+                // update loader and re-open for further pagination objects
+                $rootScope.$broadcast('spotmop:notifyUserRemoval', {id: 'loading-more-playlists'});
+                loadingMorePlaylists = false;
+            })
+            .error(function( error ){
+                $rootScope.$broadcast('spotmop:notifyUserRemoval', {id: 'loading-more-playlists'});
+                $rootScope.$broadcast('spotmop:notifyUser', {type: 'bad', id: 'loading-more-playlists', message: error.error.message});
+                loadingMorePlaylists = false;
+            });
+    }
+	
+	// once we're told we're ready to load more albums
+    $scope.$on('spotmop:loadMore', function(){
+        if( !loadingMorePlaylists && typeof( $scope.next ) !== 'undefined' && $scope.next ){
+            loadMorePlaylists( $scope.next );
+        }
+	});
 });
