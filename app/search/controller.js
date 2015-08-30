d4496aaadd8b76027311d91c6a11d9e1a9a3001a
James Barnsley <james@barnsley.nz>
2015-08-31 08:39:46 +1200

    Delaying search for 1000ms; Loader for 'all' search

--- app/search/controller.js
+++ app/search/controller.js
@@ -9,6 +9,8 @@ angular.module('spotmop.search', [])
 			url: "/search/:type/:query",
 			templateUrl: "app/search/template.html",
 			controller: 'SearchController',
+			
+			// this specifies default values if there are none defined in the URL
 			params: {
 				type: { squash: true, value: 'all' },
 				query: { squash: true, value: null }
@@ -21,13 +23,14 @@ angular.module('spotmop.search', [])
  **/
 .controller('SearchController', function SearchController( $scope, $rootScope, $state, $stateParams, $timeout, SpotifyService ){
 	
-	$scope.query = $stateParams.query;
 	$scope.tracklist = {tracks: []};
 	$scope.albums = [];
 	$scope.artists = [];
 	$scope.playlists = [];
-	$scope.type = $stateParams.type;	
+	$scope.type = $stateParams.type;
+	$scope.query = $stateParams.query;
 	$scope.loading = false;
+	var searchDelayer;
 	
 	$scope.dropdownActive = false;
 	$scope.toggleDropdown = function(){
@@ -37,34 +40,55 @@ angular.module('spotmop.search', [])
 			$scope.dropdownActive = true;
 	}
 	
+	// focus on our search field on load
+	$(document).find('.search-form input.query').focus();
+	
+	// if we've just loaded this page, and we have params, let's perform a search
+	if( $scope.query )
+		performSearch( $scope.type, $scope.query );
+	
 	/**
 	 * Watch our query string for changes
 	 * When changed, clear all results, wait for 0.5 seconds for next key, then fire off the search
 	 **/
-	$scope.$watch('[query]', function (){
-		$scope.loading = true;
-		$scope.tracklist = {tracks: []};
-		$scope.albums = [];
-		$scope.artists = [];
-		$scope.playlists = [];
-		$timeout(
-			function(){
-				//$state.go('search',{ type: $scope.type, query: $scope.query});			
-			}, 500
-		);
-	}, true);
+    var tempQuery = '', queryTimeout;
+    $scope.$watch('query', function(newValue, oldValue){
+		
+		if( newValue != oldValue && newValue && newValue != '' ){
+			$scope.loading = true;
+			$scope.tracklist = {tracks: []};
+			$scope.albums = [];
+			$scope.artists = [];
+			$scope.playlists = [];
+			
+			if (queryTimeout)
+				$timeout.cancel(queryTimeout);
+
+			tempQuery = newValue;
+			queryTimeout = $timeout(function() {
+				$scope.query = tempQuery;
+				performSearch( $scope.type, $scope.query );	
+			}, 1000);
+		}
+    })
 	
 	
 	/**
-	 * On init, figure out what type of results we want
+	 * Fetch the search results
 	 * This defines the type of search requests we'll perform, and thus the page layout
+	 * @param type = string (type of search results, all/artist/playlist/album/etc)
+	 * @param query = string
 	 **/
-	if( $scope.query ){
-		switch( $stateParams.type ){
+	function performSearch( type, query ){
+	
+		if( typeof(type) === 'undefined' )
+			var type = $scope.type;
+	
+		switch( type ){
 			
 			case 'track' :
 				$scope.loading = true;
-				SpotifyService.getSearchResults( 'track', $stateParams.query, 50 )
+				SpotifyService.getSearchResults( 'track', query, 50 )
 					.success( function(response){
 						$scope.tracklist = response.tracks;
 						$scope.tracklist.tracks = response.tracks.items;
@@ -75,7 +99,7 @@ angular.module('spotmop.search', [])
 			
 			case 'album' :
 				$scope.loading = true;
-				SpotifyService.getSearchResults( 'album', $stateParams.query, 20 )
+				SpotifyService.getSearchResults( 'album', query, 20 )
 					.success( function(response){		
 						$scope.albums = response.albums;
 						$rootScope.$broadcast('spotmop:pageUpdated');
@@ -85,7 +109,7 @@ angular.module('spotmop.search', [])
 					
 			case 'artist' :
 				$scope.loading = true;
-				SpotifyService.getSearchResults( 'artist', $stateParams.query, 20 )
+				SpotifyService.getSearchResults( 'artist', query, 20 )
 					.success( function(response){		
 						$scope.artists = response.artists;
 						$rootScope.$broadcast('spotmop:pageUpdated');
@@ -95,7 +119,7 @@ angular.module('spotmop.search', [])
 					
 			case 'playlist' :
 				$scope.loading = true;
-				SpotifyService.getSearchResults( 'playlist', $stateParams.query, 20 )
+				SpotifyService.getSearchResults( 'playlist', query, 20 )
 					.success( function(response){		
 						$scope.playlists = response.playlists;
 						$rootScope.$broadcast('spotmop:pageUpdated');
@@ -105,29 +129,46 @@ angular.module('spotmop.search', [])
 			
 			default :
 				$scope.loading = 4;
-				SpotifyService.getSearchResults( 'track', $stateParams.query, 20 )
+				SpotifyService.getSearchResults( 'track', query, 20 )
 					.success( function(response){
 						$scope.tracklist = response.tracks;
 						$scope.tracklist.tracks = response.tracks.items;
-						$scope.loading -= 1;
+						
+						// handle loading (remembering that these queries may finish in a different order)
+						$scope.loading = $scope.loading - 1;
+						if( $scope.loading <= 0 )
+							$rootScope.$broadcast('spotmop:pageUpdated');
 					});	
 					
-				SpotifyService.getSearchResults( 'album', $stateParams.query, 6 )
+				SpotifyService.getSearchResults( 'album', query, 6 )
 					.success( function(response){		
 						$scope.albums = response.albums;
-						$scope.loading -= 1;
+						
+						// handle loading (remembering that these queries may finish in a different order)
+						$scope.loading = $scope.loading - 1;
+						if( $scope.loading <= 0 )
+							$rootScope.$broadcast('spotmop:pageUpdated');
 					});
 					
-				SpotifyService.getSearchResults( 'artist', $stateParams.query, 6 )
+				SpotifyService.getSearchResults( 'artist', query, 6 )
 					.success( function(response){		
 						$scope.artists = response.artists;
-						$scope.loading -= 1;
+						
+						// handle loading (remembering that these queries may finish in a different order)
+						$scope.loading = $scope.loading - 1;
+						if( $scope.loading <= 0 )
+							$rootScope.$broadcast('spotmop:pageUpdated');
 					});
 					
-				SpotifyService.getSearchResults( 'playlist', $stateParams.query, 6 )
+				SpotifyService.getSearchResults( 'playlist', query, 6 )
 					.success( function(response){		
 						$scope.playlists = response.playlists;
-						$scope.loading -= 1;
+						
+						// handle loading (remembering that these queries may finish in a different order)
+						$scope.loading = $scope.loading - 1;
+						if( $scope.loading <= 0 )
+							$rootScope.$broadcast('spotmop:pageUpdated');
+							
 					});
 				break;
 		}
