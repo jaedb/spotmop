54edc9b44142bf2bd116490551a077f402352a91
James Barnsley <james@barnsley.nz>
2015-10-16 16:28:33 +1300

    Moving playlists into nested folders

--- app/playlists/controller.js
+++ app/playlists/controller.js
@@ -3,19 +3,42 @@ angular.module('spotmop.playlists', [])
 /**
  * Routing 
  **/
-.config(function($stateProvider) {
+.config(function($stateProvider) {		
 	$stateProvider
 		.state('playlists', {
 			url: "/playlists",
-			templateUrl: "app/playlists/template.html",
-			controller: 'PlaylistsController'
+			templateUrl: "app/playlists/template.html"
+		})
+		.state('playlists.my', {
+			url: "/my",
+			templateUrl: "app/playlists/my.template.html",
+			controller: 'MyPlaylistsController'
+		})
+		.state('playlists.following', {
+			url: "/following",
+			templateUrl: "app/playlists/following.template.html",
+			controller: 'FollowingPlaylistsController'
 		});
 })
+
 	
 /**
  * Main controller
  **/
-.controller('PlaylistsController', function PlaylistsController( $scope, $rootScope, SpotifyService, SettingsService, DialogService ){
+.controller('MyPlaylistsController', function PlaylistsController( $scope, $rootScope, $filter, SpotifyService, SettingsService, DialogService ){
+	
+	// note: we use the existing playlist list to show playlists on this page
+	
+	$scope.createPlaylist = function(){
+        DialogService.create('createPlaylist', $scope);
+	}
+})
+
+	
+/**
+ * Main controller
+ **/
+.controller('FollowingPlaylistsController', function PlaylistsController( $scope, $rootScope, SpotifyService, SettingsService, DialogService ){
 	
 	// note: we use the existing playlist list to show playlists on this page
 	
