ce34d3b2e7e59e8934593327d1913c2d2b98565b
James Barnsley <james@barnsley.nz>
2015-10-16 14:53:41 +1300

    Simplifying versionCode when on release

--- app/settings/version.php
+++ app/settings/version.php
@@ -15,9 +15,11 @@ $commit = exec('git rev-parse --short HEAD');
 // fatal response, meaning we're not on a tag (we've deviated, or 'Detached')
 if( $tag ){
 	$version = $tag;
+	$versionCode = $tag;
 }else{
 	$version = 'dev';
+	$versionCode = 'DEV-'.$commit;
 }
 
-echo json_encode(array( 'version' => $version, 'commit' => $commit, 'versionCode' => $version.'-'.$commit ));
+echo json_encode(array( 'version' => $version, 'commit' => $commit, 'versionCode' => $versionCode ));
 return;
