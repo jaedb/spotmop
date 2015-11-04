c30a4892155ac877fd6c413db9b24cd685e4de20
Mopidy Server <james@barnsley.nz>
2015-11-04 07:10:53 +0000

    Mobile player; Discover mobile

--- app/browse/featured/controller.js
+++ app/browse/featured/controller.js
@@ -24,28 +24,9 @@ angular.module('spotmop.browse.featured', [])
 	
 	// set the default items
 	$scope.playlists = [];
-	
-	// figure out the most appropriate background image to show (based on current local time)
-	$scope.partofday = function(){
-	
-		// convert to decimal (remembering that minutes are base-6)
-		var hour = parseFloat($filter('date')(new Date(),'H.m'));
-		
-		if( hour >= 4 && hour < 9.3 )
-			return 'commute';
-		else if( hour >= 9.3 && hour < 11 )
-			return 'morning';
-		else if( hour >= 11 && hour < 13.5 )
-			return 'midday';
-		else if( hour >= 13.5 && hour < 17 )
-			return 'afternoon';
-		else if( hour >= 17 && hour < 19 )
-			return 'evening';
-		else if( hour >= 19 && hour < 21 )
-			return 'dinner';
-		else if( hour >= 21 && hour < 23 || hour >= 0 && hour < 4  )
-			return 'late';
-	};
+	$scope.featured = function(){
+		return $scope.playlists[0];
+	}
 	
 	$rootScope.requestsLoading++;
 	
