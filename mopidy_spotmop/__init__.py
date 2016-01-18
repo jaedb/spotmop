fc0c64606dd7504d6fa2fefdb359678b9bef12d9
James Barnsley <james@barnsley.nz>
2016-01-19 08:18:56 +1300

    Updating version code

--- mopidy_spotmop/__init__.py
+++ mopidy_spotmop/__init__.py
@@ -10,7 +10,7 @@ from services.upgrade import upgrade
 from services.pusher import pusher
 from mopidy import config, ext
 
-__version__ = '2.5.5'
+__version__ = '2.5.6'
 __ext_name__ = 'spotmop'
 __verbosemode__ = False
 
