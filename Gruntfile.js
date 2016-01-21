98227833eab2c8fc1c8647b0237d8a0c5b17b99c
James Barnsley <james@barnsley.nz>
2016-01-22 12:12:01 +1300

    Removing excessive vendor files; Basic grunt concat and min

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,22 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            js: {
+                src: 'src/**/*.js',
+                dest: 'mopidy_spotmop/static/app.js'
+            },
+            css: {
+                src: 'src/assets/css/*.css',
+                dest: 'mopidy_spotmop/static/assets/style.css'
+            }
+        },
         uglify: {
             options: {
                 banner: '/**\n * <%= pkg.name %>\n * Built <%= grunt.template.today("yyyy-mm-dd") %>\n **/\n\n'
             },
             build: {
-                src: 'src/app/app.js',
+                src: 'mopidy_spotmop/static/app.js',
                 dest: 'mopidy_spotmop/static/app.min.js'
             }
         }
@@ -16,8 +26,9 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['concat', 'uglify']);
 
 };
