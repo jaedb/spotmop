4cb9fd6889100375a05acdad1923353e37b2ea0c
James Barnsley <james@barnsley.nz>
2016-02-16 08:08:31 +1300

    Next release number; Lookup for local track objects

--- src/app/library/controller.js
+++ src/app/library/controller.js
@@ -154,99 +154,6 @@ angular.module('spotmop.library', [])
 })
 	
 /**
- * Local files
- **/
-.controller('LibraryFilesController', function ( $scope, $rootScope, $filter, $stateParams, SpotifyService, SettingsService, DialogService, MopidyService ){
-	
-	$scope.folders = [];
-	$scope.tracklist = {tracks: [], type: 'local'};
-	
-	var folder, parentFolder;
-	
-	if( $stateParams.folder ){
-	
-		folder = $stateParams.folder;
-		var parentFolders = folder.split('|');
-		parentFolder = '';
-		for( var i = 0; i < parentFolders.length-1; i++ ){
-			showParentFolderLink = true;
-			parentFolder += parentFolders[i];
-			if( i < parentFolders.length-2 )
-				parentFolder += '|';
-		}
-		
-		if( parentFolder == '' )
-			parentFolder = 'local:directory';
-		
-		folder = folder.replace('|','/');
-	}
-	
-	// on init, go get the items (or wait for mopidy to be online)
-	if( $scope.mopidyOnline )
-		getItems();
-	else
-		$scope.$on('mopidy:state:online', function(){ getItems() });
-	
-	
-	// go get em
-	function getItems(){
-			
-		MopidyService.getLibraryItems( folder )
-			.then( function( response ){
-					
-					// load tracks
-					var tracks = $filter('filter')(response, {type: 'track'});	
-/*					
-					if( tracks.length > 0 ){
-						
-						for( var i = 0; i < tracks.length; i++ ){
-							MopidyService.getTrack( tracks[i].uri )
-								.then( function(response){
-									console.log( i );
-									tracks[i] = response;
-									console.log( response );
-								});
-						}
-						
-					}*/
-					
-					$scope.tracklist.tracks = tracks;
-					
-					// fetch the folders
-					var folders = formatFolders( $filter('filter')(response, {type: 'directory'}) );
-
-					if( $stateParams.folder != 'local:directory' )
-						folders.unshift({ name: '..', uri: parentFolder, type: 'directory', isParentFolder: true });
-					
-					$scope.folders = folders;
-				});
-	}
-	
-	
-	/**
-	 * Format our folders into the desired format
-	 * @param items = array
-	 * @return array
-	 **/
-	function formatFolders( items ){
-		
-		// sanitize uris
-		for( var i = 0; i < items.length; i++ ){
-			var item = items[i];
-			
-			// replace slashes (even urlencoded ones) to ":"
-			item.uri = item.uri.replace('%2F', '|');
-			item.uri = item.uri.replace('/', '|');
-			
-			items[i] = item;
-		}
-		
-		return items;
-	}
-		
-})
-	
-/**
  * Library artists
  **/
 .controller('LibraryArtistsController', function ( $scope, $rootScope, $filter, SpotifyService, SettingsService, DialogService ){
@@ -356,6 +263,108 @@ angular.module('spotmop.library', [])
             loadMorePlaylists( $scope.playlists.next );
         }
 	});
+})
+
+
+	
+/**
+ * Local files
+ **/
+.controller('LibraryFilesController', function ( $scope, $rootScope, $filter, $stateParams, SpotifyService, SettingsService, DialogService, MopidyService ){
+	
+	$scope.folders = [];
+	$scope.tracklist = {tracks: [], type: 'local'};
+	
+	var folder, parentFolder;
+	
+	if( $stateParams.folder ){
+	
+		folder = $stateParams.folder;
+		var parentFolders = folder.split('|');
+		parentFolder = '';
+		for( var i = 0; i < parentFolders.length-1; i++ ){
+			showParentFolderLink = true;
+			parentFolder += parentFolders[i];
+			if( i < parentFolders.length-2 )
+				parentFolder += '|';
+		}
+		
+		if( parentFolder == '' )
+			parentFolder = 'local:directory';
+		
+		folder = folder.replace('|','/');
+	}
+	
+	// on init, go get the items (or wait for mopidy to be online)
+	if( $scope.mopidyOnline )
+		getItems();
+	else
+		$scope.$on('mopidy:state:online', function(){ getItems() });
+	
+	
+	// go get em
+	function getItems(){
+			
+		MopidyService.getLibraryItems( folder )
+			.then( function( response ){
+			
+					// load tracks
+					var trackReferences = $filter('filter')(response, {type: 'track'});
+					var trackUris = [];
+					
+					// loop all the track references, so we can get all the track objects
+					for( var i = 0; i < trackReferences.length; i++ ){
+						trackUris.push( trackReferences[i].uri );
+					}
+					
+					// take our track references and look up the actual track objects
+					if( trackUris.length > 0 ){
+						MopidyService.getTracks( trackUris )
+							.then( function( response ){
+							
+								var tracks = [];
+								
+								// loop all the tracks to sanitize the response
+								for( var key in response ){
+									tracks.push( response[key][0] );
+								}
+								
+								$scope.tracklist.tracks = tracks;
+							});
+					}
+					
+					// fetch the folders
+					var folders = formatFolders( $filter('filter')(response, {type: 'directory'}) );
+
+					if( $stateParams.folder != 'local:directory' )
+						folders.unshift({ name: '..', uri: parentFolder, type: 'directory', isParentFolder: true });
+					
+					$scope.folders = folders;
+				});
+	}
+	
+	
+	/**
+	 * Format our folders into the desired format
+	 * @param items = array
+	 * @return array
+	 **/
+	function formatFolders( items ){
+		
+		// sanitize uris
+		for( var i = 0; i < items.length; i++ ){
+			var item = items[i];
+			
+			// replace slashes (even urlencoded ones) to ":"
+			item.uri = item.uri.replace('%2F', '|');
+			item.uri = item.uri.replace('/', '|');
+			
+			items[i] = item;
+		}
+		
+		return items;
+	}
+		
 });
 
 
