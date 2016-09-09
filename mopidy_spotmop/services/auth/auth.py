7ed29e9f07b7c3dc5398cd63ff65b452cba2c2ec
James Barnsley <james@barnsley.nz>
2016-09-10 08:40:41 +1200

    Tidier structure to auth, nicer to get token from backend...

--- mopidy_spotmop/services/auth/auth.py
+++ mopidy_spotmop/services/auth/auth.py
@@ -6,17 +6,9 @@ import urllib, urllib2, json
 from tornado.escape import json_encode
 import subprocess
 
-class AuthRequestHandler(tornado.web.RequestHandler):
+class AuthHelper(object):
     
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-
-    def initialize(self, core, config):
-        self.core = core
-        self.config =  config
-	
-    def get(self):
-
+    def get_token( self ):
         url = 'https://accounts.spotify.com/api/token'
         authorization = 'YTg3ZmI0ZGJlZDMwNDc1YjhjZWMzODUyM2RmZjUzZTI6ZDdjODlkMDc1M2VmNDA2OGJiYTE2NzhjNmNmMjZlZDY='
 
@@ -33,11 +25,22 @@ class AuthRequestHandler(tornado.web.RequestHandler):
         try:
             response = urllib2.urlopen(req, timeout=30).read()
             response_dict = json.loads(response)
-            self.write(response_dict)
-            return
+            return response_dict
         except urllib2.HTTPError as e:
             return e
 
+class AuthRequestHandler(tornado.web.RequestHandler):
+    
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+
+    def initialize(self, core, config):
+        self.core = core
+        self.config =  config
+	
+    def get(self):
+        self.write( AuthHelper().get_token() )
+        return
 
 def spotmop_auth_factory(config, core):
     return [
