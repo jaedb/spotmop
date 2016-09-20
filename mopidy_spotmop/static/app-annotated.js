7d6a903926dac54fa2fa246d9bbb19ca7854af00
James Barnsley <james@barnsley.nz>
2016-09-20 19:21:48 +1200

    Youtube track title = name to resolve #143, Source icons

--- mopidy_spotmop/static/app-annotated.js
+++ mopidy_spotmop/static/app-annotated.js
@@ -33100,7 +33100,7 @@ angular.module('spotmop.common.track', [])
 	return {
 		restrict: 'E',
 		templateUrl: 'app/common/tracklist/track.template.html',
-		controller: ["$element", "$scope", "$rootScope", "MopidyService", "NotifyService", "PlayerService", function( $element, $scope, $rootScope, MopidyService, NotifyService, PlayerService ){
+		controller: ["$element", "$scope", "$rootScope", "$filter", "MopidyService", "NotifyService", "PlayerService", function( $element, $scope, $rootScope, $filter, MopidyService, NotifyService, PlayerService ){
 			
             // parse our parent tracklist into the track itself
             // useful for detecting drag event capabilities
@@ -33113,23 +33113,13 @@ angular.module('spotmop.common.track', [])
 			$scope.isCurrentlyPlaying = function(){
 				return ( typeof($scope.track.tlid) !== 'undefined' && $scope.track.tlid == $scope.state().currentTlTrack.tlid );
 			}
-			
-			/**
-			 * What type of track are we? Use our uri to figure this out
-			 * @return string
-			 **/
-			$scope.sourceIconClasses = function(){
-                if( typeof($scope.track.uri) !== 'undefined' ){
-                    var source = $scope.track.uri.split(':')[0];
-                    var state = 'light';
-                    if( $scope.isCurrentlyPlaying() ){
-                        if( source == 'spotify' ) state = 'green';
-                        if( source == 'local' ) state = 'yellow';
-                        if( source == 'soundcloud' ) state = 'red';
-                    }
-                    return source +' '+ state;
-                }
-			}
+            
+            // get source for this track (ie spotify, youtube, local)
+            $scope.source = function(){
+                var source = $filter('assetOrigin')( $scope.track.uri );
+                if( source == 'local' || source == 'file' ) source = 'folder';
+                return source;
+            }
 			
 			/**
 			 * Single click
@@ -35591,9 +35581,13 @@ angular.module('spotmop.search', [])
 		$scope.results.playlists = [];
 		
 		// perform the mopidy search
+        console.log('Performing search for '+query);
+        
 		MopidyService.search(fields, query, sources)
 			.then( function(sources){
 				
+                console.log( sources );
+                
 				for( var i = 0; i < sources.length; i++ ){
 					var source = sources[i];
 					
